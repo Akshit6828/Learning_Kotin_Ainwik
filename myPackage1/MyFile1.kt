cdf53cf1e09a3137867fa2f43e32ca4b39773f4c
Akshit Mangotra <akshitmangotra@gmail.com>
2021-05-28 20:52:51 +0530

    Added Package Accessing in OPPS

---
+++ myPackage1/MyFile1.kt
@@ -0,0 +1,9 @@
+package myPackage1
+
+open class C1{
+    open fun Amy()
+    {
+        println("Funtion  called...")
+    }
+
+}
---
+++ myPackage1/MyFile2.kt
@@ -0,0 +1,8 @@
+package myPackage1
+
+import myPackage1.C1//this is the package imported here.
+//   Package name------> ClassName.
+fun main() {
+    val obj=C1()//Association
+    obj.Amy()
+}
